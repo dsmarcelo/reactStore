598a43fdfcdec468b20015d37d56cbfcb17a1db5
dsmarcelo <celoygm@gmail.com>
2022-09-29 12:51:55 -0300

    feat: Implement DB Seeding

---
+++ back-end/seed/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'Wireless Gaming Mouse',
+    price: 150,
+    category:
+  },
+  {
+    name: 'Oled TV 65"',
+    price: 1799,
+    category:
+  },
+  {
+    name: 'Coffee Maker',
+    price: 50,
+    category:
+  },
+  {
+    name: 'White Shirt',
+    price: 20,
+    category: 
+  },
+]
