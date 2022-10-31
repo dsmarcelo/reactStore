c0d62aa0290cb849dea9dbfd3fc88e7ce29f4f2c
dsmarcelo <celoygm@gmail.com>
2022-10-31 11:13:53 -0300

    feat: add slug to categories

--- back-end/seed/categories.ts
+++ back-end/seed/categories.ts
@@ -1,18 +1,22 @@
 export const categories = [
   {
     name: 'Computers',
+    slug: 'computers',
     image: ''
   },
   {
     name: 'Home Appliances',
+    slug: 'home-appliances',
     image: ''
   },
   {
     name: 'Clothing',
+    slug: 'clothing',
     image: ''
   },
   {
     name: 'Smartphones',
+    slug: 'smartphones',
     image: ''
   },
 ]
