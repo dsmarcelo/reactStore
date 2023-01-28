ae1b596f1301e03125abf83e1e981dcf33e254cc
dsmarcelo <celoygm@gmail.com>
2023-01-28 16:05:09 -0300

    refactor: remove comments

--- src/components/homeCarousel.tsx
+++ src/components/homeCarousel.tsx
@@ -5,31 +5,22 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
-// import 'swiper/swiper.scss'
 
 export default function HomeCarousel() {
   const [items, setItems] = useState<string[]>([]);
 
   function fetchImages() {
     const imagesQty = 3
-    let images: string[] = []
+    const images: string[] = []
     for (let i = 0; i < imagesQty; i++) {
       images.push(`/carousel/carousel-ad-${i}.jpg`)
     }
-    console.log('🚀 ~ fetchImages ~ images', images);
     return images;
   }
 
   useEffect(() => {
     setItems(fetchImages())
   }, [])
-
-  // const params = {
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     clickable: true
-  //   }
-  // }
 
   return (
     <Swiper
