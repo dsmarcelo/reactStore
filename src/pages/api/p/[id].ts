6579a858a9405c71ba500d0b2b03448cc9021b54
dsmarcelo <celoygm@gmail.com>
2023-12-08 09:48:25 -0300

    feat: add correct status codes do /api/p/[id]

--- src/pages/api/p/[id].ts
+++ src/pages/api/p/[id].ts
@@ -6,31 +6,26 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { method, query } = req;
+  const { id } = query;
+  if (typeof id !== 'string' || !id) {
+    res.status(400).json({ message: 'ID must be a hex string' });
+    res.end();
+  }
 
   if (method === 'GET') {
-    const { id } = query;
     try {
-      if (!id) {
-        res.status(404).json({ message: 'id not provided' });
+      const product = await prisma.product.findFirst({
+        where: {
+          id: id as string,
+        },
+      });
+
+      if (!product) {
+        res.status(404).json({ message: 'Product not found' });
         res.end();
       }
 
-      if (typeof id !== 'string') {
-        res.status(400).json({ message: 'Incorrect id type' });
-      }
-
-      if (typeof id === 'string') {
-        const product = await prisma.product.findFirst({
-          where: {
-            id,
-          },
-        });
-
-        if (!product) {
-          res.status(404).json({ message: 'Product not found' });
-        }
-        res.status(200).json({ product });
-      }
+      res.status(200).json({ product });
     } catch (error) {
       res.status(500).json({
         message: 'An error occurred',
