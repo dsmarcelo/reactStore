ea5cca3a0f50a406263c6502a63212f5e7d10fa8
dsmarcelo <celoygm@gmail.com>
2024-01-31 19:17:10 -0300

    chore: change password for user seeds

--- back-end/seed/users.ts
+++ back-end/seed/users.ts
@@ -2,13 +2,13 @@ export const users = [
   {
     name: 'Mario',
     email: 'mario@email.com',
-    password: '123',
+    password: '$2b$10$nFtSKIqIbLdDvleF7sGoSOghczl3Lx0XHOQDOcnWnmkFqRROOwN8.',
     role: 'customer',
   },
   {
     name: 'Maria',
     email: 'admin@email.com',
-    password: '123',
+    password: '$2b$10$nFtSKIqIbLdDvleF7sGoSOghczl3Lx0XHOQDOcnWnmkFqRROOwN8.',
     role: 'admin',
   },
 ];
