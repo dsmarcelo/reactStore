83ba66df54f320fbd77206ad73414e6f507105c5
dsmarcelo <celoygm@gmail.com>
2024-01-31 22:07:29 -0300

    fix: install @prisma/nextjs-monorepo-workaround-plugin

--- src/next.config.js
+++ src/next.config.js
@@ -1,9 +1,18 @@
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["api.lorem.space", "www.google.com", "placeimg.com"]
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
   },
 }
 
