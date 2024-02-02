018e27150def32fea23712324306eed7b3a9d1fe
dsmarcelo <celoygm@gmail.com>
2024-02-02 15:52:19 -0300

    fix: add /api/u/auth.ts

--- src/pages/api/auth/options.ts
+++ src/pages/api/auth/options.ts
@@ -2,13 +2,8 @@ import { NextAuthOptions } from 'next-auth';
 import CredentialProvider from 'next-auth/providers/credentials';
 import { prisma } from '../../../lib/prisma';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import bcryp from 'bcrypt';
 
 export const authOptions: NextAuthOptions = {
-  // adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: 'jwt',
-  },
   providers: [
     CredentialProvider({
       name: 'Credentials',
@@ -22,24 +17,29 @@ export const authOptions: NextAuthOptions = {
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        if (!credentials) {
-          return null;
-        }
+        if (!credentials) return null;
 
         const { email, password } = credentials;
 
-        const user = await prisma.user.findUnique({ where: { email } });
-
+        const user = await fetch(`${process.env.NEXTAUTH_URL}/api/u/auth`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        })
+          .then((res) => res.json())
+          .catch((err) => {
+            console.error(err);
+            return null;
+          });
         if (!user) return null;
-
-        const isMatch = await bcryp.compare(password, user.password);
-
-        if (!isMatch) {
-          return null;
-        }
 
         return user;
       },
     }),
   ],
+  // adapter: PrismaAdapter(prisma),
+  session: { strategy: 'jwt' },
+  secret: process.env.NEXTAUTH_SECRET,
 };
---
+++ src/pages/api/u/auth.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../lib/prisma';
+import bcrypt from 'bcrypt';
+
+//TODO: Add image to user and remove password in the res.json
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Missing required fields' });
+  }
+
+  try {
+    let user = await prisma.user.findUnique({
+      where: { email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: true,
+      },
+    });
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        return res.status(200).json(exclude(user, ['password']));
+      }
+    } else {
+      res.status(401).json({ message: 'Incorrect credentials' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
