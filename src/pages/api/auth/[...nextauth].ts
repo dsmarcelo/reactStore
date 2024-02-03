f952281fd71695526cdb50275d4e147da287c2ff
dsmarcelo <celoygm@gmail.com>
2024-02-03 18:51:55 -0300

    chrore: change nextath options

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,43 @@
 import NextAuth from 'next-auth';
-import { authOptions } from './options';
+import CredentialProvider from 'next-auth/providers/credentials';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '../../../lib/prisma';
 
-export default NextAuth(authOptions);
+export default NextAuth({
+  // adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialProvider({
+      name: 'Credentials',
+
+      credentials: {
+        email: {
+          label: 'email',
+          type: 'text',
+          placeholder: 'email@email.com',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+
+        const { email, password } = credentials;
+
+        const user = await fetch(`${process.env.NEXTAUTH_URL}/api/u/auth`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        }).then((res) => res.json());
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: { strategy: 'jwt' },
+  secret: process.env.NEXTAUTH_SECRET,
+});
