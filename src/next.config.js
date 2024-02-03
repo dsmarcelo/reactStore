62d62f8e3a52b82091bb8dcc361eb8364b533c03
dsmarcelo <celoygm@gmail.com>
2024-02-03 09:37:11 -0300

    build: change nextjs-monorepo-workaround import

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,4 @@
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -16,4 +16,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+export default nextConfig
